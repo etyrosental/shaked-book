a8e2802a24d9fb8ae43d290c5cd44467d67ec75e
Aviv Rosental <rosental.aviv@gmail.com>
2015-12-28 23:06:10 +0200

    add website content

---
+++ index.js
@@ -0,0 +1,10 @@
+ // video (from QR)
+ // about the book (credits etc..)
+ // about the author
+ // contact form
+ // buy online, list of stores..
+ // future (games..)
+
+ $(document).ready(function() {
+     $('#fullpage').fullpage();
+ });
